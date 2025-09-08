bf8b7deb44d254c6a81cd0fa97802a964e090e6a
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-09-08 12:52:48 -0300

    chore(db): configurar conexão PostgreSQL e criar teste de persistência Estado

---
+++ backend/src/main/java/com/atlasdigital/controllers/TestController.java
@@ -0,0 +1,10 @@
+package com.atlasdigital.controllers;
+
+/**
+ * Author: Bruno Miguel Correa
+ * Email: devsistemascorrea@gmail.com
+ * Data: 08/09/2025
+**/
+
+ public class TestController {
+}
---
+++ backend/src/main/java/com/atlasdigital/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.atlasdigital.repositories;
+
+/**
+ * Author: Bruno Miguel Correa
+ * Email: devsistemascorrea@gmail.com
+ * Data: 08/09/2025
+**/
+
+ public class EstadoRepository {
+}
